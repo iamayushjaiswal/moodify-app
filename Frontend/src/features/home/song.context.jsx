823f2298596666451374e94f85497acb47145be8
Ayush Jaiswal <ayush.003754@snuindia.in>
2026-07-04 14:25:13 +0530

    Remove default Lady Singham song

--- Frontend/src/features/home/song.context.jsx
+++ Frontend/src/features/home/song.context.jsx
@@ -5,12 +5,7 @@ export const SongContext = createContext()
 
 export const SongContextProvider = ({ children }) => {
 
-    const [ song, setSong ] = useState({
-        "url": "https://ik.imagekit.io/hnoglyswo0/cohort-2/moodify/songs/Lady_Singham_gs01DFz-1.mp3",
-        "posterUrl": "https://ik.imagekit.io/hnoglyswo0/cohort-2/moodify/posters/Lady_Singham_VW8DGJkie.jpeg",
-        "title": "Lady Singham",
-        "mood": "happy",
-    })
+    const [ song, setSong ] = useState(null)
 
     const [ loading, setLoading ] = useState(false)
 
